e5ea70ab7b9ac9da31958aed23f2875b9d91784b
Hyowon83 <aile1383@gmail.com>
2021-06-19 14:37:26 +0900

    .

--- inSAT/practice/과제.py
+++ inSAT/practice/과제.py
@@ -146,31 +146,31 @@
 # print("영어 평균: ",engTotal/allStudent)
 # print("수학 평균: ",mathTotal/allStudent)
 
-#2 깔끔한 버전
-# students = []
-# scores = [0.0, 0.0, 0.0, 0.0]
-# ENG_TOTAL = 0
-# MATH_TOTAL = 1
-# ENG_AVG = 2
-# MATH_AVG = 3
+#2
+students = []
+scores = [0.0, 0.0, 0.0, 0.0]
+ENG_TOTAL = 0
+MATH_TOTAL = 1
+ENG_AVG = 2
+MATH_AVG = 3
 
 
-# while True:
-#     name = input("이름: ")
-#     if name == "":
-#         break
-#     mat = float(input("수학 점수: "))
-#     eng = float(input("영어 점수: "))
-#     students.append({"이름": name, "수학 점수": mat, "영어 점수": eng})
+while True:
+    name = input("이름: ")
+    if name == "":
+        break
+    mat = float(input("수학 점수: "))
+    eng = float(input("영어 점수: "))
+    students.append({"이름": name, "수학 점수": mat, "영어 점수": eng})
 
-# for student in students:
-#     scores[MATH_TOTAL] += student["수학 점수"]
-#     scores[ENG_TOTAL] += student["영어 점수"]
+for student in students:
+    scores[MATH_TOTAL] += student["수학 점수"]
+    scores[ENG_TOTAL] += student["영어 점수"]
 
-# scores[MATH_AVG] = scores[MATH_TOTAL] / len(students)
-# scores[ENG_AVG] = scores[ENG_TOTAL] / len(students)
+scores[MATH_AVG] = scores[MATH_TOTAL] / len(students)
+scores[ENG_AVG] = scores[ENG_TOTAL] / len(students)
 
-# print("수학 점수의 총합: {}".format(scores[MATH_TOTAL]))
-# print("영어 점수의 총합: {}".format(scores[ENG_TOTAL]))
-# print("수학 점수의 평균: {}".format(scores[MATH_AVG]))
-# print("영어 점수의 평균: {}".format(scores[ENG_AVG]))
+print("수학 점수의 총합: {}".format(scores[MATH_TOTAL]))
+print("영어 점수의 총합: {}".format(scores[ENG_TOTAL]))
+print("수학 점수의 평균: {}".format(scores[MATH_AVG]))
+print("영어 점수의 평균: {}".format(scores[ENG_AVG]))
