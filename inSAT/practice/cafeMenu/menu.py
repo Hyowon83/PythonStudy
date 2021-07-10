b25480576a89c21ead23f98c92e797fe1a7b4a1c
Hyowon83 <aile1383@gmail.com>
2021-07-10 14:27:43 +0900

    .split .join 등등

--- inSAT/practice/cafeMenu/menu.py
+++ inSAT/practice/cafeMenu/menu.py
@@ -14,20 +14,39 @@
 #             print(self.menuList[i],self.priceList[i])
 
 
+# import os
+# filePath = "c:/tmp"
+#         if os.path.isfile("c:/tmp/menu.txt"):
+#             mode = "a"
+#         else:
+#             mode = "w"
 
 #### saveList 추가(파일 저장)
 class Menu:
     def __init__(self):
         self.menuList = []
         self.priceList = []
-    def addMenu(self,menuName,menuPrice):
-        self.menuList.append(menuName)
-        self.priceList.append(menuPrice)
+    def addMenu(self, name, price):
+        self.menuList.append(name)
+        self.priceList.append(price)
     def printList(self):
         for i in range(len(self.menuList)):
             print(self.menuList[i],self.priceList[i])
     def saveList(self):
-        self.file = open("D:/menu.txt","w")
-        for i in range(len(self.menuList)):
-            self.file.write("{}: {}\n".format(self.menuList[i],self.priceList[i]))
-        self.file.close
+        with open("D:/tmp/menu.txt", "w") as file:
+            for i in range(len(self.menuList)):
+                file.write("{}, {}\n".format(self.menuList[i], self.priceList[i]))
+
+
+# sentence = "카라멜마끼아또/2000원"
+# # 비파괴적 함수, 파괴적 함수
+# print(sentence.split("/"))
+# print(sentence)
+
+# sentence = sentence[1:]
+# print(sentence[1:])
+# print(sentence)
+
+# print("Hello World", seq="")
+
+
