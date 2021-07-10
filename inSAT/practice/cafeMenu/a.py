aa569a7cf83836baa79141706c88ea87c5b20d04
Hyowon83 <aile1383@gmail.com>
2021-07-10 12:36:26 +0900

    입력받은 메뉴 출력 및 txt 저장

--- inSAT/practice/cafeMenu/a.py
+++ inSAT/practice/cafeMenu/a.py
@@ -1,18 +1,35 @@
-import menu as m
+#### 수정 및 보안(상운오빠)
+# import menu as m
 
-# menu = m.Menu()
-# menu.getMenu()
-# menu.addMenu(menu.name, menu.price)
-# menu.printList()
+# # menu = m.Menu()
+# # menu.getMenu()
+# # menu.addMenu(menu.name, menu.price)
+# # menu.printList()
 
 
-cafe = m.Menu()
+# cafe = m.Menu()
 
-while True:
-    cafe.name = input("메뉴 이름:")
-    if cafe.name == "":
-        break
-    cafe.price = int(input("가격: "))
-    cafe.addMenu(cafe.name,cafe.price)
+# while True:
+#     cafe.name = input("메뉴 이름:")
+#     if cafe.name == "":
+#         break
+#     cafe.price = int(input("가격: "))
+#     cafe.addMenu(cafe.name,cafe.price)
+
+# cafe.printList()
+
+
+
+#### 박재형 선생님
+import menu
+
+cafe = menu.Menu()
+
+name = input("메뉴명 : ")
+while name != "":
+    price = input("가격 : ")
+    cafe.addMenu(name,price)
+    name = input("메뉴명 : ")
 
 cafe.printList()
+cafe.saveList()
--- inSAT/practice/cafeMenu/menu.py
+++ inSAT/practice/cafeMenu/menu.py
@@ -1,13 +1,33 @@
+#### 수정 및 보안(상운오빠)
+# class Menu:
+#     def __init__(self):
+#         self.menuList = []
+#         self.priceList = []
+#     # def getMenu(self):
+#     #     self.name = input("메뉴명 : ")
+#     #     self.price = input("가격 : ")
+#     def addMenu(self,menuName,menuPrice):
+#         self.menuList.append(menuName)
+#         self.priceList.append(menuPrice)
+#     def printList(self):
+#         for i in range(len(self.menuList)):
+#             print(self.menuList[i],self.priceList[i])
+
+
+
+#### 박재형 선생님
 class Menu:
     def __init__(self):
         self.menuList = []
         self.priceList = []
-    # def getMenu(self):
-    #     self.name = input("메뉴명 : ")
-    #     self.price = input("가격 : ")
     def addMenu(self,menuName,menuPrice):
         self.menuList.append(menuName)
         self.priceList.append(menuPrice)
     def printList(self):
         for i in range(len(self.menuList)):
             print(self.menuList[i],self.priceList[i])
+    def saveList(self):
+        self.file = open("D:/menu.txt","w")
+        for i in range(len(self.menuList)):
+            self.file.write("{},{}\n".format(self.menuList[i],self.priceList[i]))
+        self.file.close
